9da4cd8f2b188e215852070ae3b492abedf4eb4b
Xiaoyuvax <xiaoyu.vax@qq.com>
2019-03-22 14:14:18 +0800

    A GUI interface added

---
+++ Passwd/frmMain.Designer.cs
@@ -0,0 +1,178 @@
+﻿namespace DomainPasswd
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtDomainUser = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtOldPwd = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtNewPwd = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtConfirmPwd = new System.Windows.Forms.TextBox();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.btnOK = new System.Windows.Forms.Button();
+            this.chkSSL = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(60, 53);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(101, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Domain\\&Username:";
+            // 
+            // txtDomainUser
+            // 
+            this.txtDomainUser.Location = new System.Drawing.Point(167, 50);
+            this.txtDomainUser.Name = "txtDomainUser";
+            this.txtDomainUser.Size = new System.Drawing.Size(154, 21);
+            this.txtDomainUser.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(78, 94);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(83, 12);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "&Old password:";
+            // 
+            // txtOldPwd
+            // 
+            this.txtOldPwd.Location = new System.Drawing.Point(167, 91);
+            this.txtOldPwd.Name = "txtOldPwd";
+            this.txtOldPwd.Size = new System.Drawing.Size(154, 21);
+            this.txtOldPwd.TabIndex = 2;
+            this.txtOldPwd.UseSystemPasswordChar = true;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(78, 131);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(83, 12);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "&New password:";
+            // 
+            // txtNewPwd
+            // 
+            this.txtNewPwd.Location = new System.Drawing.Point(167, 128);
+            this.txtNewPwd.Name = "txtNewPwd";
+            this.txtNewPwd.Size = new System.Drawing.Size(154, 21);
+            this.txtNewPwd.TabIndex = 3;
+            this.txtNewPwd.UseSystemPasswordChar = true;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(30, 167);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(131, 12);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "Confirm new &password:";
+            // 
+            // txtConfirmPwd
+            // 
+            this.txtConfirmPwd.Location = new System.Drawing.Point(167, 164);
+            this.txtConfirmPwd.Name = "txtConfirmPwd";
+            this.txtConfirmPwd.Size = new System.Drawing.Size(154, 21);
+            this.txtConfirmPwd.TabIndex = 4;
+            this.txtConfirmPwd.UseSystemPasswordChar = true;
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Location = new System.Drawing.Point(246, 217);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(75, 23);
+            this.btnCancel.TabIndex = 6;
+            this.btnCancel.Text = "&Cancel";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
+            // 
+            // btnOK
+            // 
+            this.btnOK.Location = new System.Drawing.Point(156, 217);
+            this.btnOK.Name = "btnOK";
+            this.btnOK.Size = new System.Drawing.Size(75, 23);
+            this.btnOK.TabIndex = 5;
+            this.btnOK.Text = "O&K";
+            this.btnOK.UseVisualStyleBackColor = true;
+            this.btnOK.Click += new System.EventHandler(this.btnOK_Click);
+            // 
+            // chkSSL
+            // 
+            this.chkSSL.AutoSize = true;
+            this.chkSSL.Location = new System.Drawing.Point(213, 12);
+            this.chkSSL.Name = "chkSSL";
+            this.chkSSL.Size = new System.Drawing.Size(108, 16);
+            this.chkSSL.TabIndex = 7;
+            this.chkSSL.Text = "SSL Connection";
+            this.chkSSL.UseVisualStyleBackColor = true;
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(370, 274);
+            this.Controls.Add(this.chkSSL);
+            this.Controls.Add(this.btnOK);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.txtConfirmPwd);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtNewPwd);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtOldPwd);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtDomainUser);
+            this.Controls.Add(this.label1);
+            this.Name = "frmMain";
+            this.Text = "Change Domain Password";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtDomainUser;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtOldPwd;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtNewPwd;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtConfirmPwd;
+        private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.Button btnOK;
+        private System.Windows.Forms.CheckBox chkSSL;
+    }
+}
---
+++ Passwd/frmMain.cs
@@ -0,0 +1,55 @@
+﻿using Passwd;
+using Passwd.SystemDirectoryServicesImpl;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Security;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DomainPasswd
+{
+    public partial class frmMain : Form
+    {
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnOK_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (txtNewPwd.Text == txtConfirmPwd.Text)
+                {
+                    using (SecureString oldPwd = new SecureString(), newPwd = new SecureString())
+                    {
+                        txtOldPwd.Text.ToCharArray().ToList().ForEach(i => oldPwd.AppendChar(i));
+                        txtNewPwd.Text.ToCharArray().ToList().ForEach(i => newPwd.AppendChar(i));
+
+                        if (Program.ExecuteFromCommandLine(new string[] { (chkSSL.Checked ? " -s" : ""), "-u " + txtDomainUser.Text }, false, oldPwd, newPwd) == 0)
+                            MessageBox.Show("Password changed successfully!", "Done!", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                        else
+                            MessageBox.Show("Changing password failed!\r\r" + Program.Log, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                }
+                else throw new Exception("New password not match!");
+
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+}
